77db5f1e2fc8d6c6883ec91e4f08db93c50a17f6
bochka123 <sasha2003tkach001100@gmail.com>
2023-05-22 18:21:44 +0300

    added header and footer

--- frontend/src/app/modules/news/news.module.ts
+++ frontend/src/app/modules/news/news.module.ts
@@ -1,9 +1,10 @@
 import {NewsPageComponent} from "./news-page/news-page.component";
 import {NgModule} from "@angular/core";
 import {NewsRoutingModule} from "./news-routing.module";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
   declarations: [NewsPageComponent],
-  imports: [NewsRoutingModule],
+  imports: [NewsRoutingModule, SharedModule],
 })
 export class NewsModule {}
